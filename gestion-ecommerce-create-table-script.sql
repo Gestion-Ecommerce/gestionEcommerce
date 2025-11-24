1dbc5112edbf63172933353bc6bcacf531cdf008
María <119685476+mariaruizpaton@users.noreply.github.com>
2025-11-24 20:09:21 +0100

    [ADD] script creacion tablas y clases model

---
+++ gestion-ecommerce-create-table-script.sql
@@ -0,0 +1,66 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS informacion_fiscal;
+DROP TABLE IF EXISTS clientes;
+DROP TABLE IF EXISTS compras;
+DROP TABLE IF EXISTS articulos;
+DROP TABLE IF EXISTS articulo_compra;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+-- Información fiscal
+CREATE TABLE IF NOT EXISTS informacion_fiscal(
+	nif_cif VARCHAR(9) PRIMARY KEY NOT NULL UNIQUE,
+	telefono VARCHAR(9) NOT NULL UNIQUE,
+	direccion VARCHAR(150) NOT NULL
+)
+ENGINE=InnoDB;
+
+-- Relacion 1:1 con información fiscal
+CREATE TABLE IF NOT EXISTS clientes (
+	nif_cif VARCHAR(9) PRIMARY KEY NOT NULL UNIQUE,
+	nombre_completo VARCHAR(100) NOT NULL UNIQUE,
+	email VARCHAR(150) NOT NULL UNIQUE,
+	fecha_creacion DATETIME,
+	FOREIGN KEY (nif_cif) REFERENCES informacion_fiscal(nif_cif)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
+)
+ENGINE=InnoDB;
+
+-- Relacion N:M Clientes <-> Compras
+CREATE TABLE IF NOT EXISTS compras (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	id_cliente VARCHAR(9) NOT NULL UNIQUE,
+	fecha_compra DATETIME NOT NULL,
+	estado ENUM('Pendiente', 'Enviado', 'Entregado') NOT NULL,
+	precio_total DECIMAL NOT NULL,
+	FOREIGN KEY (id_cliente) REFERENCES clientes(nif_cif)
+	ON DELETE CASCADE
+)
+ENGINE=InnoDB;
+
+-- Articulos
+CREATE TABLE IF NOT EXISTS articulos (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(100) NOT NULL,
+	descripcion VARCHAR(150),
+	precio_actual DECIMAL NOT NULL,
+	stock INT NOT NULL
+)
+ENGINE=InnoDB;
+
+-- Articulo_Compra
+-- Relacion N:M
+CREATE TABLE IF NOT EXISTS articulo_compra (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	id_compra INT NOT NULL,
+	id_articulo INT NOT NULL,
+	unidades INT NOT NULL,
+	precio_compra DECIMAL NOT NULL,
+	FOREIGN KEY (id_compra) REFERENCES compras(id)
+	ON DELETE CASCADE,
+	FOREIGN KEY (id_articulo) REFERENCES articulos(id)
+	ON DELETE CASCADE
+)
+ENGINE=InnoDB;
