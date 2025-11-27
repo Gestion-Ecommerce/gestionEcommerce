554c9aa9193b70ef4e1f3a75931b04c134041e9c
María <119685476+mariaruizpaton@users.noreply.github.com>
2025-11-27 16:06:05 +0100

    [MOD] clase main y persistence.xml

--- src/main/java/com/marialiviu/u3/gestionEcommerce/App.java
+++ src/main/java/com/marialiviu/u3/gestionEcommerce/App.java
@@ -1,10 +1,37 @@
 package com.marialiviu.u3.gestionEcommerce;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    	// Carga la configuración del persistence.xml
+    	EntityManagerFactory emf = null;
+    	EntityManager em = null;
+    	
+    	try {
+			emf = Persistence.createEntityManagerFactory("ecommerce-jpa-pu");
+			em = emf.createEntityManager();
+			
+			//TODO
+			
+		} catch (Exception e) {
+			System.err.println("¡Error en la transacción!");
+			e.printStackTrace();
+			
+		} finally {
+			if(em != null) {
+				em.close();
+			}
+			if(emf != null) {
+				emf.close();
+			}
+			System.out.println("\n Aplicación finalizada");
+		}
     }
 }
