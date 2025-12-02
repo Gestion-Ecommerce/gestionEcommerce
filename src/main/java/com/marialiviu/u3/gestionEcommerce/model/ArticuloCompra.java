6d6d22ff6dda1dd42d9010438041bb321ec8c0e3
Liviu <ldrotariu01@gmail.com>
2025-12-02 17:42:50 +0100

    [MOD] documentacion completa articulocompra

--- src/main/java/com/marialiviu/u3/gestionEcommerce/model/ArticuloCompra.java
+++ src/main/java/com/marialiviu/u3/gestionEcommerce/model/ArticuloCompra.java
@@ -38,12 +38,10 @@ import java.util.Objects;
  * </p>
  * 
  * <pre>{@code
- * Articulo a = new Articulo();
- * c.setId("1");
- * c.setNombre("Pan");
- * c.setDescripcion("Pan integral");
- * c.setPrecioActual(1.99f);
- * c.setStock(100);
+ * Articulo articulo = new Articulo(...);
+ * Compra compra = new Compra(...);
+ * ArticuloCompra articuloCompra = new ArticuloCompra(compra, articulo, 5, 19.99f);
+ * System.out.println(articuloCompra);
  * }</pre>
  *
  * @author Liviu
@@ -57,28 +55,49 @@ import java.util.Objects;
 @Table(name = "articulo_compra")
 public class ArticuloCompra {
 
+	/**
+	 * Identificador de la compra.
+	 */
     @Id
     @Column(name = "id_compra")
     private Integer compraId;
 
+    /**
+     * Identificador del artículo.
+     */
     @Id
     @Column(name = "id_articulo")
     private Integer articuloId;
 
+    /**
+     * Referencia a la entidad Compra asociada.
+     */
     @ManyToOne
     @JoinColumn(name = "id_compra", referencedColumnName = "id", insertable = false, updatable = false)
     private Compra compra;
 
+    /**
+     * Referencia a la entidad Artículo asociada.
+     */
     @ManyToOne
     @JoinColumn(name = "id_articulo", referencedColumnName = "id", insertable = false, updatable = false)
     private Articulo articulo;
 
+    /**
+     * Cantidad de unidades del artículo en la compra.
+     */
     @Column(name = "unidades")
     private int unidades;
 
+    /**
+     * Precio del artículo en el momento de la compra.
+     */
     @Column(name = "precio_compra")
     private float precioCompra;
 
+    /**
+     * Constructor por defecto que inicializa los campos a valores predeterminados.
+     */
     public ArticuloCompra() {
         this.compraId = null;
         this.articuloId = null;
@@ -88,6 +107,14 @@ public class ArticuloCompra {
         this.precioCompra = 0;
     }
 
+    /**
+     * Constructor parametrizado para crear una instancia de ArtículoCompra con
+     * valores específicos.
+     * @param compra
+     * @param articulo
+     * @param unidades
+     * @param precioCompra
+     */
     public ArticuloCompra(Compra compra, Articulo articulo, int unidades, float precioCompra) {
         this();
         this.compra = compra;
@@ -98,56 +125,106 @@ public class ArticuloCompra {
         this.precioCompra = (precioCompra >= 0) ? precioCompra : 0;
     }
 
+    /**
+     * Obtiene el identificador de la compra.
+     * @return
+     */
     public Integer getCompraId() {
         return compraId;
     }
 
+    /**
+     * Establece el identificador de la compra.
+     * @param compraId
+     */
     public void setCompraId(Integer compraId) {
         this.compraId = compraId;
     }
 
+    /**
+     * Obtiene el identificador del artículo.
+     * @return
+     */
     public Integer getArticuloId() {
         return articuloId;
     }
 
+    /**
+     * Establece el identificador del artículo.
+     * @param articuloId
+     */
     public void setArticuloId(Integer articuloId) {
         this.articuloId = articuloId;
     }
 
+    /**
+     * Obtiene la compra asociada.
+     * @return
+     */
     public Compra getCompra() {
         return compra;
     }
 
+    /**
+     * Establece la compra asociada.
+     * @param compra
+     */
     public void setCompra(Compra compra) {
         this.compra = compra;
         this.compraId = (compra != null) ? compra.getId() : null;
     }
 
+    /**
+     * Obtiene el artículo asociado.
+     * @return
+     */
     public Articulo getArticulo() {
         return articulo;
     }
-
+    /**
+     * Establece el artículo asociado.
+     * @param articulo
+     */
     public void setArticulo(Articulo articulo) {
         this.articulo = articulo;
         this.articuloId = (articulo != null) ? articulo.getId() : null;
     }
 
+    /**
+     * Obtiene la cantidad de unidades del artículo en la compra.
+     * @return
+     */
     public int getUnidades() {
         return unidades;
     }
 
+    /**
+     * Establece la cantidad de unidades del artículo en la compra.
+     * @param unidades
+     */
     public void setUnidades(int unidades) {
         this.unidades = (unidades >= 0) ? unidades : 0;
     }
 
+    /**
+     * Obtiene el precio del artículo en el momento de la compra.
+     * @return
+     */
     public float getPrecioCompra() {
         return precioCompra;
     }
 
+    /**
+     * Establece el precio del artículo en el momento de la compra.
+     * @param precioCompra
+     */
     public void setPrecioCompra(float precioCompra) {
         this.precioCompra = (precioCompra >= 0) ? precioCompra : 0;
     }
 
+    /**
+     * Devuelve una representación en cadena del objecto ArtículoCompra.
+     */
     @Override
     public String toString() {
         int idC = (compraId != null) ? compraId : 0;
@@ -156,11 +233,18 @@ public class ArticuloCompra {
                 + ", precioCompra=" + precioCompra + "]";
     }
 
+    /**
+     * Genera un código hash basado en los identificadores de compra y artículo.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(compraId, articuloId);
     }
 
+    /**
+     * Compara este objeto con otro para determinar igualdad basada en los
+     * identificadores de compra y artículo.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
